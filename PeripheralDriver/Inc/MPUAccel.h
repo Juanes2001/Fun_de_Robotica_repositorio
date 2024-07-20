2746b6a5bc6eb5f124f327dabfd548f0e2180436
Juanes2001 <111066079+Juanes2001@users.noreply.github.com>
2024-07-20 13:13:46 -0500

    MPU Gyro Config start

--- PeripheralDriver/Inc/MPUAccel.h
+++ PeripheralDriver/Inc/MPUAccel.h
@@ -16,30 +16,7 @@
 /* Definiciones para funcionamiento del acelerometro */
 #define ADDRESS_UP          0b1101001
 #define ADDRESS_DOWN        0b1101000
-#define ACCEL_XOUT_H 		59
-#define ACCEL_XOUT_L		60
-#define ACCEL_YOUT_H		61
-#define ACCEL_YOUT_L		62
-#define ACCEL_ZOUT_H		63
-#define ACCEL_ZOUT_L		64
 
-#define GYRO_XOUT_H 		67
-#define GYRO_XOUT_L			68
-#define GYRO_YOUT_H			69
-#define GYRO_YOUT_L			70
-#define GYRO_ZOUT_H			71
-#define GYRO_ZOUT_L			72
-
-#define PWR_MGMT_l			107
-#define WHO_AM_I			117
-
-void writeAccelValues(I2C_Handler_t *ptrHandlerI2C, uint8_t address , uint8_t newValue);
-uint8_t readAccelValues(I2C_Handler_t *ptrHandlerI2C, uint8_t address);
-void startAccel(I2C_Handler_t *ptrHandlerI2C);
-int16_t X_Accel(I2C_Handler_t *ptrHandlerI2C);
-int16_t Y_Accel(I2C_Handler_t *ptrHandlerI2C);
-int16_t X_Gyro(I2C_Handler_t *ptrHandlerI2C);
-int16_t Y_Gyro(I2C_Handler_t *ptrHandlerI2C);
 
 
 #endif /* MPUACCEL_H_ */
